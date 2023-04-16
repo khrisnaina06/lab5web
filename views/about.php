9eab717a5a0f4650cfb1442a656cf7c82c6e084b
khrisnaina06 <muhammadkhrisna12@gmail.com>
2023-04-16 07:52:54 +0700

    my duty

---
+++ views/about.php
@@ -0,0 +1,11 @@
+<div class="w-full p-5 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+  <img class="w-24 h-24 m-auto mb-4 rounded-full shadow-lg" src="https://avatars.githubusercontent.com/u/93519687?v=4" alt="Bonnie image" />
+  <h5 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">Muhammad khrisna</h5>
+  <p class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">
+  Nama saya adalah Muhammad Khrisna faisal zuhri, saat ini sedang menempuh pendidikan S1 di jurusan Teknik Informatika. Saya memiliki minat dan passion yang kuat dalam teknologi dan komputer sejak kecil, sehingga saya memilih untuk mengejar karir di bidang ini.
+</p>
+  <p class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">Selain kuliah, saya juga aktif menjadi guru ngaji di TPA Baitul Atiq. Menjadi guru ngaji merupakan salah satu kegiatan yang saya jalani selama beberapa tahun terakhir. Saya merasa senang dan bangga dapat membantu memperkenalkan agama Islam kepada anak-anak muda di lingkungan sekitar, serta memberikan pengetahuan dan pengalaman tentang keislaman mereka.</p>
+
+  <p class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">Saya memiliki keterampilan dalam bahasa pemrograman seperti Java dan Python, serta pengalaman dalam pengembangan aplikasi mobile. Saya juga terampil dalam penggunaan perangkat lunak seperti Photoshop dan AutoCAD. Selain itu, saya juga aktif dalam kegiatan ekstrakurikuler seperti membuat website dan aplikasi untuk kegiatan organisasi kampus.
+  </p>
+</div>
